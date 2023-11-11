8ab37bcc3764648e3462d1860a202dfa10047192
SinaAbdollahi79 <sina.7979ab@gmail.com>
2023-11-11 15:01:15 +0330

    set data base
    set first query on data base

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class post(models.Model):
+    titel = models.CharField(max_length=100)
+    content = models.TextField()
+    conted_view = models.IntegerField(default=0)
+    status =models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True )
+    updated_date = models.DateTimeField(auto_now=True )
+
+
+  
