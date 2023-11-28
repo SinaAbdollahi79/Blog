3dde9fa6d387367ba923d1972fefb4f99c8c7783
SinaAbdollahi79 <sina.7979ab@gmail.com>
2023-11-29 02:41:31 +0330

    update project

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-from blog.models import post
+from blog.models import post,categories
 
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
     empty_value_display = ""
-    list_display = ("titel", "status", "created_date", "published_date")
+    list_display = ("titel","author", "status", "created_date", "published_date")
     list_filter = ("status", "created_date", "published_date")
     #ordering = ["-created_date"]
     search_fields = ("titel","status", "created_date")
 
 
 admin.site.register(post, PostAdmin)
+admin.site.register(categories)
+
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class categories(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
 class post(models.Model):
     titel = models.CharField(max_length=100)
+    author = models.ForeignKey(User , on_delete=models.SET_NULL , null=True)
     content = models.TextField()
+    image = models.ImageField(upload_to='blog/' ,default='blog/default.png')
+    category = models.ManyToManyField(categories)
     conted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
