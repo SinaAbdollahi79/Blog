4677611c6da65f0cd4dd9cd3c9fdcb59e65d27a3
SinaAbdollahi79 <sina.7979ab@gmail.com>
2024-01-30 18:36:26 +0330

    gjv
    ghvhg

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from website.forms import ContactForm
 from django.contrib import messages
+from django.conf import settings
 
 
 def home(request):
@@ -17,8 +18,8 @@ def contact(request):
             messages.success(request, '.تیکت شما با موفقیعت ثبت شد')
         else:
             messages.error(request, '.متاسفانه تیکت شما ثبت نشد')
-            
+    google_maps_api_key = settings.GOOGLE_MAPS_API_KEY        
     form = ContactForm()
 
-    return render(request, 'website/contact.html' , {'form': form})
+    return render(request, 'website/contact.html' , {'form': form ,'google_maps_api_key': google_maps_api_key})
 
